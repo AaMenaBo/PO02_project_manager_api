dd73a81420537682d017c6d3c395262b4c3f58ec
Aaron Mena <aamenabo@est.utn.ac.cr>
2025-04-05 14:20:40 -0600

    Update tasks migration to use foreignIdFor for user and project references; set user_id to nullable with onDelete set null for improved data integrity.

--- database/migrations/2025_03_26_002421_create_tasks_table.php
+++ database/migrations/2025_03_26_002421_create_tasks_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Project;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,8 +17,8 @@ return new class extends Migration
             $table->id();
             $table->text('description');
             $table->text('status');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('project_id')->constrained()->onDelete('cascade');
+            $table->foreignIdFor(User::class)->nullable()->constrained()->onDelete('set null');
+            $table->foreignIdFor(Project::class)->constrained()->onDelete('cascade');
         });
     }
 
